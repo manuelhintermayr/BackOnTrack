3b4a44b6e3ea0b57d4f8c97a91c6cabbef91068a
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-11-07 02:02:32 +0000

    added modern ui view

--- BackOnTrack/BackOnTrack/MainView/MainView.xaml.cs
+++ BackOnTrack/BackOnTrack/MainView/MainView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,19 +13,21 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows.Controls;
 
 namespace BackOnTrack.MainView
 {
     /// <summary>
     /// Interaction logic for MainView.xaml
     /// </summary>
-    public partial class MainView : Window
+    public partial class MainView : ModernWindow
     {
         private MainWindow mainWindow;
         public MainView(MainWindow mainWindow)
         {
             InitializeComponent();
             this.mainWindow = mainWindow;
+            Application.Current.Resources["AccentColor"] = Colors.Teal;
             //Closing += OnWindowClosing();
         }
 
