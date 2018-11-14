d5a4ed110814e381286ae7313c32ef434fef4261
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-11-14 18:53:47 +0000

    added empty WebProxy and ProgramConfiguration Services

--- BackOnTrack/BackOnTrack/Services/ServicesKeyword.cs
+++ BackOnTrack/BackOnTrack/Services/ServicesKeyword.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BackOnTrack.Services.ProgramConfiguration;
+using BackOnTrack.Services.Proxy;
 using BackOnTrack.Services.UserConfiguration;
 
 namespace BackOnTrack.Services
 {
     public class ServicesKeyword
     {
-        public UserConfigurationSupplier UserConfiguration; 
+        public UserConfigurationSupplier UserConfiguration;
+        public ProgramConfigurationSupplier ProgramConfiguration;
+        public WebProxy WebProxy;
         public ServicesKeyword()
         {
-            UserConfiguration = new UserConfigurationSupplier();;
+            UserConfiguration = new UserConfigurationSupplier();
+            ProgramConfiguration = new ProgramConfigurationSupplier();
+            WebProxy = new WebProxy();
         }
     }
 }
