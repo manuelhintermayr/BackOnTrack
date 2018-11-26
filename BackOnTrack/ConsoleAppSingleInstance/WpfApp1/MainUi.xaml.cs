e52fbbae14e1a9b0b747923ed24edeb732c20306
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-11-26 22:02:13 +0000

    Moved ui from application start to own WPF-Window

---
+++ BackOnTrack/ConsoleAppSingleInstance/WpfApp1/MainUi.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainUi.xaml
+    /// </summary>
+    public partial class MainUi : Window
+    {
+        bool minimizedToTray;
+        NotifyIcon notifyIcon;
+        private MainWindow _mainWindow;
+
+        public MainUi(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+
+            //HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            //source.AddHook(new HwndSourceHook(WndProc));
+        }
+
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == SingleInstance.WM_SHOWFIRSTINSTANCE)
+            {
+                ShowWindow();
+            }
+            //  do stuff
+
+            return IntPtr.Zero;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MinimizeToTray();
+        }
+
+
+        void MinimizeToTray()
+        {
+            notifyIcon = new NotifyIcon();
+            //notifyIcon.Click += new EventHandler(NotifyIconClick);
+            notifyIcon.DoubleClick += new EventHandler(NotifyIconClick);
+            notifyIcon.Icon = new System.Drawing.Icon("logo.ico");
+            notifyIcon.Text = this.Title;
+            notifyIcon.Visible = true;
+            this.WindowState = WindowState.Minimized;
+            this.Hide();
+            minimizedToTray = true;
+        }
+        public void ShowWindow()
+        {
+            if (minimizedToTray)
+            {
+                notifyIcon.Visible = false;
+                this.Show();
+                this.WindowState = WindowState.Normal;
+                minimizedToTray = false;
+            }
+            else
+            {
+                WinApi.ShowToFront(new WindowInteropHelper(this).Handle);
+            }
+        }
+        void NotifyIconClick(Object sender, System.EventArgs e)
+        {
+            ShowWindow();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            _mainWindow.Shutdown();
+        }
+    }
+
+    static public class WinApi
+    {
+        [DllImport("user32")]
+        public static extern int RegisterWindowMessage(string message);
+
+        public static int RegisterWindowMessage(string format, params object[] args)
+        {
+            string message = String.Format(format, args);
+            return RegisterWindowMessage(message);
+        }
+
+        public const int HWND_BROADCAST = 0xffff;
+        public const int SW_SHOWNORMAL = 1;
+
+        [DllImport("user32")]
+        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void ShowToFront(IntPtr window)
+        {
+            ShowWindow(window, SW_SHOWNORMAL);
+            SetForegroundWindow(window);
+        }
+    }
+
+    static public class ProgramInfo
+    {
+        static public string AssemblyGuid
+        {
+            get
+            {
+                object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(System.Runtime.InteropServices.GuidAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return String.Empty;
+                }
+                return ((System.Runtime.InteropServices.GuidAttribute)attributes[0]).Value;
+            }
+        }
+        static public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().CodeBase);
+            }
+        }
+    }
+
+    static public class SingleInstance
+    {
+        public static readonly int WM_SHOWFIRSTINSTANCE =
+            WinApi.RegisterWindowMessage("WM_SHOWFIRSTINSTANCE|{0}", ProgramInfo.AssemblyGuid);
+        static Mutex mutex;
+        static public bool Start()
+        {
+            bool onlyInstance = false;
+            string mutexName = String.Format("Local\\{0}", ProgramInfo.AssemblyGuid);
+
+            // if you want your app to be limited to a single instance
+            // across ALL SESSIONS (multiple users & terminal services), then use the following line instead:
+            // string mutexName = String.Format("Global\\{0}", ProgramInfo.AssemblyGuid);
+
+            mutex = new Mutex(true, mutexName, out onlyInstance);
+            return onlyInstance;
+        }
+        static public void ShowFirstInstance()
+        {
+            WinApi.PostMessage(
+                (IntPtr)WinApi.HWND_BROADCAST,
+                WM_SHOWFIRSTINSTANCE,
+                IntPtr.Zero,
+                IntPtr.Zero);
+        }
+        static public void Stop()
+        {
+            mutex.ReleaseMutex();
+        }
+    }
+}
--- BackOnTrack/ConsoleAppSingleInstance/WpfApp1/MainWindow.xaml.cs
+++ BackOnTrack/ConsoleAppSingleInstance/WpfApp1/MainWindow.xaml.cs
@@ -27,154 +27,23 @@ namespace WpfApp1
     {
         bool minimizedToTray;
         NotifyIcon notifyIcon;
+        private MainUi mainUi;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            //HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
-            //source.AddHook(new HwndSourceHook(WndProc));
-        }
-
-        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            if (msg == SingleInstance.WM_SHOWFIRSTINSTANCE)
-            {
-                ShowWindow();
-            }
-            //  do stuff
-
-            return IntPtr.Zero;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MinimizeToTray();
-        }
-
-
-        void MinimizeToTray()
-        {
-            notifyIcon = new NotifyIcon();
-            //notifyIcon.Click += new EventHandler(NotifyIconClick);
-            notifyIcon.DoubleClick += new EventHandler(NotifyIconClick);
-            notifyIcon.Icon = new System.Drawing.Icon("logo.ico");
-            notifyIcon.Text = this.Title;
-            notifyIcon.Visible = true;
-            this.WindowState = WindowState.Minimized;
             this.Hide();
-            minimizedToTray = true;
+            mainUi = new MainUi(this);
+            mainUi.Show();
         }
-        public void ShowWindow()
+
+        public void Shutdown()
         {
-            if (minimizedToTray)
-            {
-                notifyIcon.Visible = false;
-                this.Show();
-                this.WindowState = WindowState.Normal;
-                minimizedToTray = false;
-            }
-            else
-            {
-                WinApi.ShowToFront(new WindowInteropHelper(this).Handle);
-            }
+            mainUi.Hide();
+            Environment.Exit(0);
         }
-        void NotifyIconClick(Object sender, System.EventArgs e)
-        {
-            ShowWindow();
-        }
+
     }
 
-    static public class WinApi
-    {
-        [DllImport("user32")]
-        public static extern int RegisterWindowMessage(string message);
-
-        public static int RegisterWindowMessage(string format, params object[] args)
-        {
-            string message = String.Format(format, args);
-            return RegisterWindowMessage(message);
-        }
-
-        public const int HWND_BROADCAST = 0xffff;
-        public const int SW_SHOWNORMAL = 1;
-
-        [DllImport("user32")]
-        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
-
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        [DllImport("user32.dll")]
-        public static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        public static void ShowToFront(IntPtr window)
-        {
-            ShowWindow(window, SW_SHOWNORMAL);
-            SetForegroundWindow(window);
-        }
-    }
-
-    static public class ProgramInfo
-    {
-        static public string AssemblyGuid
-        {
-            get
-            {
-                object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(System.Runtime.InteropServices.GuidAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return String.Empty;
-                }
-                return ((System.Runtime.InteropServices.GuidAttribute)attributes[0]).Value;
-            }
-        }
-        static public string AssemblyTitle
-        {
-            get
-            {
-                object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-                if (attributes.Length > 0)
-                {
-                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                    if (titleAttribute.Title != "")
-                    {
-                        return titleAttribute.Title;
-                    }
-                }
-                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().CodeBase);
-            }
-        }
-    }
-
-    static public class SingleInstance
-    {
-        public static readonly int WM_SHOWFIRSTINSTANCE =
-            WinApi.RegisterWindowMessage("WM_SHOWFIRSTINSTANCE|{0}", ProgramInfo.AssemblyGuid);
-        static Mutex mutex;
-        static public bool Start()
-        {
-            bool onlyInstance = false;
-            string mutexName = String.Format("Local\\{0}", ProgramInfo.AssemblyGuid);
-
-            // if you want your app to be limited to a single instance
-            // across ALL SESSIONS (multiple users & terminal services), then use the following line instead:
-            // string mutexName = String.Format("Global\\{0}", ProgramInfo.AssemblyGuid);
-
-            mutex = new Mutex(true, mutexName, out onlyInstance);
-            return onlyInstance;
-        }
-        static public void ShowFirstInstance()
-        {
-            WinApi.PostMessage(
-                (IntPtr)WinApi.HWND_BROADCAST,
-                WM_SHOWFIRSTINSTANCE,
-                IntPtr.Zero,
-                IntPtr.Zero);
-        }
-        static public void Stop()
-        {
-            mutex.ReleaseMutex();
-        }
-    }
+    
 }
