4cd05f3f6cb0e9ac6932e558f8c2d6da7477da90
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-12-03 21:33:16 +0000

    added option to load correct userControl when selecting a specific profile

---
+++ BackOnTrack/BackOnTrack/UI/MainView/Pages/Profiles/ProfileLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirstFloor.ModernUI.Windows;
+
+namespace BackOnTrack.UI.MainView.Pages.Profiles
+{
+    public class ProfileLoader : DefaultContentLoader
+    {
+        protected override object LoadContent(Uri uri)
+        {
+            return new SpecificProfileView(uri.ToString());
+        }
+    }
+}
