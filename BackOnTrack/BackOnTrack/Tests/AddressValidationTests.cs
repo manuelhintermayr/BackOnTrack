8d1386f9e05a79c401c66046fd975dfc1870c861
Manuel Hintermayr <manuel@hintermayr.net>
2019-02-24 21:05:07 +0000

    created empty base for AddressValidationTests()

---
+++ BackOnTrack/BackOnTrack/Tests/AddressValidationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackOnTrack.SharedResources.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackOnTrack.Tests
+{
+    [TestClass]
+    public class AddressValidationTests : TestBase
+    {
+        [TestMethod]
+        public void IsCorrectAddress()
+        {
+            //
+        }
+
+        [TestMethod]
+        public void IsCorrectRegex()
+        {
+            //
+        }
+
+        [TestMethod]
+        public void IsAddressAlreadyUsed()
+        {
+            //
+        }
+    }
+}
