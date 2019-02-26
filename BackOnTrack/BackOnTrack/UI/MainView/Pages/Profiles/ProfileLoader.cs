d53c3d66bb2b0835ba3c455a52a0961462b1f8c4
Manuel Hintermayr <manuel@hintermayr.net>
2019-02-26 12:20:34 +0000

    could not found a todo for that

--- BackOnTrack/BackOnTrack/UI/MainView/Pages/Profiles/ProfileLoader.cs
+++ BackOnTrack/BackOnTrack/UI/MainView/Pages/Profiles/ProfileLoader.cs
@@ -18,7 +18,7 @@ namespace BackOnTrack.UI.MainView.Pages.Profiles
             }
             catch (System.InvalidOperationException)
             {
-                //happens when specificProfileView is reloaded but old specificProfileView is still in editing mode ==> see todo for that
+                //happens when specificProfileView is reloaded but old specificProfileView is still in editing mode
                 string alertTitle = "Could not load profile.";
                 string alertContent =
                     "Could not load profile, try opening the page again with the menu. If is doesn`t work, save your changes, log out and back in.";
